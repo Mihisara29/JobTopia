deb59283c70475c0b12926e33e6273ada869a11f
Mihisara29 <146804234+Mihisara29@users.noreply.github.com>
2026-04-12 23:27:12 +0530

    Created Job view secction.

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Routing\Route as RoutingRoute;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/jobs',function(){
-    return '<h1>Available Jobs</h1>';
+    return view('jobs.index');
 })->name('jobs');
 
-Route::get('/posts/{id}/comments/{commentId}',function(string $id,string $commentId){
-    return 'Post ' .$id . 'Comment' . $commentId;
+Route::get('/jobs/create',function(){
+    return view('jobs.create');
+})->name('jobs.create');
+
+Route::get('/test',function(){
+    return response()->json(['name'=>'John Doe'])->cookie('name','John Doe');
+});
+
+Route::get('/download',function(){
+    return response()->download(public_path('favicon.ico'));
+});
+
+Route::get('/read-cookie',function(Request $request){
+    $cookieValue = $request->cookie('name');
+    return response()->json(['cookie' => $cookieValue]);
 });
